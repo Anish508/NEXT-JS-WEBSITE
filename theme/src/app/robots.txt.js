0ee5a52d7a1382d68b3da31c5339a26db6f2873b
ShrinidhiM13 <shrinidhim9168@gmail.com>
2025-09-08 13:52:18 +0530

    seo

--- theme/src/app/robots.txt.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  res.setHeader("Content-Type", "text/plain");
-  res.send(`
-User-agent: *
-Allow: /
-Sitemap: https://bodhify.tech/sitemap.xml
-  `);
-}
