0e506d0a354939d3e68685b685a031e0deab8900
Anish508 <anishbarke9741@gmail.com>
2025-09-08 16:40:21 +0530

    modified:   theme/src/app/login/page.tsx

--- theme/src/app/login/page.tsx
+++ theme/src/app/login/page.tsx
@@ -14,7 +14,7 @@ import {
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/Card";
+} from "../../components/ui/Card";
 import { Button } from "@/components/ui/Button";
 import { Input } from "@/components/ui/Input";
 import { ButtonLoader, FullPageLoader } from "@/components/ui/Loader";
